d05d303cc7ff2fb28d9769ce79cfedd0a0c40d1e
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-10-07 21:17:56 -0300

    criando lista simples na memória

--- lib/home_paga.dart
+++ lib/home_paga.dart
@@ -8,8 +8,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controllerTarefa = TextEditingController();
+  final List<String> _tarefas = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de tarefas', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.all(8),
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: _controllerTarefa,
+                decoration: InputDecoration(
+                  labelText: 'Digite uma tarefa',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira uma tarefa';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              if (_formKey.currentState!.validate()) {
+                setState(() {
+                  _tarefas.add(_controllerTarefa.text);
+                  _controllerTarefa.clear();
+                });
+              }
+            },
+            child: Container(
+              width: double.infinity,
+              height: 40,
+              color: Colors.indigo,
+              margin: EdgeInsets.only(left: 8, right: 8),
+              child: Center(
+                child: Text(
+                  'Adicionar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_tarefas[index]));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
